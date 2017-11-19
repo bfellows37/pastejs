735a3d989bc0448d8a3971e601b7d67bf1f46c81
brad <bfellows@gmail.com>
2017-11-18 19:13:27 -0800

    / route, remove cookie if error getting posts before redirecting to login

--- docker-install.sh
+++ docker-install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+
+# put the site down
 docker-compose down
+
+# remove old images and containers
+docker rm $(docker ps -aq)
+docker rmi $(docker images -q) --force
+
+# install frontend assets........ TODO maybe do this in the container?
 cd frontend/src/assets
 ../../node_modules/.bin/bower install
+
+# put the site up
 cd ../../..
 docker-compose up
