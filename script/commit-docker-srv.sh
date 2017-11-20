b729e75e62652a8806dd1f94f3e6d75064b1c076
brad <bfellows@gmail.com>
2017-11-19 18:22:53 -0800

    add docker commit scripts

--- script/commit-docker-srv.sh
+++ script/commit-docker-srv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker-compose -f docker-compose-dev.yml build --no-cache srv
-docker tag paste_app bfellows37/pasteapi
+docker tag paste_srv bfellows37/pasteapi
 docker push bfellows37/pasteapi
